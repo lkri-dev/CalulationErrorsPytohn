2fd7308e00016a640c5c6b37d9a81cf2dbc9f369
lkri <lkri@techcollege.dk>
2021-12-16 14:39:08 +0100

    Opgaver færdige med fejl i udregningerne.

---
+++ Opgave1_main.py
@@ -0,0 +1,17 @@
+"""
+Tips
+Press Shift+F10 to execute it or replace it with your code.
+Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+Dette program er lavet til at øve fejlfinding i kode.
+Her er et program som skal udregne BMI baseret på en vægt i kg og en højde i m, det giver er ferkert resuiltat. Hvorfor?
+"""
+
+def bmi_udregner(vaegt, hoejde):
+    return (hoejde * hoejde) / vaegt
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    kg = input('Indtast en vægt i kg')
+    m = input('Indtast en højde i m')
+    print(bmi_udregner(kg, m))
